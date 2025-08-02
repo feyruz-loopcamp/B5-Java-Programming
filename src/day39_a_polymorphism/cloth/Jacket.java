f00f5cda1601239c39f0b47de75532e8f28bd5ed
feyruz-loopcamp <feyruz@loopcamp.io>
2025-08-02 15:17:34 -0400

    Polymorphism, Exception Intro

---
+++ src/day39_a_polymorphism/cloth/Jacket.java
@@ -0,0 +1,14 @@
+package day39_a_polymorphism.cloth;
+// CONCRETE Class
+public class Jacket extends Clothes implements HasHood {
+
+    @Override
+    public void wearClothes() {
+        System.out.println("Wearing Jacket...");
+    }
+
+    @Override
+    public void putOnHoody() {
+        System.out.println("Putting on hoody...");
+    }
+}
